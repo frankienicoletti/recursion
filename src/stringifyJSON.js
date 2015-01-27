5dc5fa41c9e3b3402ce54d90a403d0444a4811c6
Kate Jefferson <barbedbella@gmail.com>
2015-01-26 20:09:20 -0600

    stringifyJSON is done

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,60 +5,54 @@
 
 var stringifyJSON = function(obj) {
 
-    /*console.log(obj);
-    var stringifyJSON = JSON.stringify(obj);
-    console.log(stringifyJSON);
-    return stringifyJSON;*/
-
+    // declare variables
     var stringified;
     var objType;
 
-    if (obj === undefined) {
-        objType = undefined;
-    } else if (obj === 'undefined') {
-        objType = undefined;
-    } else if (obj === function(){}) {
-        objType = undefined;
-    } else if (obj === 'function..') {
-        objType = undefined;
-    } else if (obj === null) {
-        objType = null;
-    } else {
-        objType = typeof obj;
-    }
-    console.log(obj);
-    console.log(objType);
+    // determine object type
+    // exclude undefined
+    if (obj === undefined) return;
 
-    if (objType === undefined) {
-        stringified = "";
-    } else if (objType === null) {
-        stringified = 'null';
-    } else if (obj.constructor === Array) {
+    if (obj === null)
+        return 'null';
+    else
+        objType = typeof obj;
+
+    // functions to be excluded
+    if (objType == "function") return;
+
+    // action items
+    if (obj.constructor === Array) {
+
+        // iterate over array
         for (var i=0;i<obj.length;i++) {
             obj[i] = stringifyJSON(obj[i]);
         }
+
+        // return string of array
         stringified = "[" + obj + "]";
+
     } else if (obj.constructor === Object) {
-        console.log("This is an Object");
-         var objString = "";
+
+        var objString = "";
+
+        // empty objects
         if (Object.getOwnPropertyNames(obj).length === 0 ) {
             console.log("In if");
             stringified = "{}";
             return stringified;
         } else {
+
             var i = 0;
+
+            // iterate over object
             for (var key in obj) {
-
-                console.log(obj[key]);
-
-
-                if (key === undefined || key == 'function..') {
-                        key = "";
-                        obj[key] = "";
-                } else {
-
+                // exclude undefined and function key value pairs
+                if (!(obj[key] === undefined || typeof obj[key] == "function")) {
+                    // stringify object key value pair
                     obj[key] = '\"' + key + '\"' + ":" + stringifyJSON(obj[key]);
 
+                    //add appropriate commas and semi-colons
                     if (i < 1) {
                         objString = objString + obj[key];
                     } else {
@@ -66,19 +60,20 @@ var stringifyJSON = function(obj) {
                     }
                     i++;
                 }
-
-                console.log(obj[key]);
             }
+
             stringified = "{" + objString + "}";
         }
-        console.log(stringified);
     } else if (objType === "string") {
+
         stringified = '\"'+ obj + '\"';
+
     } else {
+
         stringified = obj.toString();
+
     }
 
-    console.log("RESULT: " + stringified);
     return stringified;
 
 };
